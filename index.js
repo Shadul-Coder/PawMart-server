a18594714e3ee408f78e106e70cb4d9bf30ef9ee
Shadul-Coder <codershadul@gmail.com>
2025-11-11 22:15:25 +0600

    Completed Get Functionality

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@shadul.twf8c9s.mongodb.net/?appName=SHADUL`;
 
@@ -21,10 +25,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    const pawmartDB = client.db("pawmartdb");
+    const petssupplies = pawmartDB.collection("pets_and_supplies");
+
+    app.get("/pets&supplies", async (req, res) => {
+      const cursor = petssupplies
+        .find()
+        .project({ name: 1, category: 1, price: 1, location: 1, image: 1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/pets&supplies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await petssupplies.findOne(query);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
