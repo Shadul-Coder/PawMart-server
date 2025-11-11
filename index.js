fa996b8b9acead629cd1f162e51aa7bf8fa1fda6
Shadul-Coder <codershadul@gmail.com>
2025-11-11 09:39:17 +0600

    Connected MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@shadul.twf8c9s.mongodb.net/?appName=SHADUL`;
 
 app.get("/", (req, res) => {
   res.send("PawMart Server Running");
 });
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port);
